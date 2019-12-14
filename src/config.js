f6564612fc9e37427904657d6182eabddbf65617
Clifford Tran <clipqq>
2019-12-13 21:07:37 -0800

    edit API_ENDPOINT to deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: `https://fuudi-server.herokuapp.com/`,
+  // API_ENDPOINT: 'http://localhost:8000',
   TOKEN_KEY: 'fuudi-client-auth-token',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
 }
