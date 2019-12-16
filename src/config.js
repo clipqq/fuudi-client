8a44a15a86e728e291fbdb83ab89de10dcf3df50
Clifford Tran <clipqq>
2019-12-16 14:04:05 -0800

    changed to prod endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_ENDPOINT: `https://fuudi-server.herokuapp.com/`,
-  // API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: ` https://intense-badlands-26980.herokuapp.com`,
   TOKEN_KEY: 'fuudi-client-auth-token',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
 }
